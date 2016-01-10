c74882d1e00681a70f67d62f905db941b5aefee8
Chandrasekhar Allam <shekar.shine@gmail.com>
2016-01-10 14:25:40 +0530

    logic added

--- app/src/main/java/shekar/com/notificationscheduler/SampleAlarmReceiver.java
+++ app/src/main/java/shekar/com/notificationscheduler/SampleAlarmReceiver.java
@@ -15,13 +15,14 @@ import java.util.Calendar;
  * and then starts the IntentService {@code SampleSchedulingService} to do some work.
  */
 public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
+    private static final int startTime=6;
     // The app's AlarmManager, which provides access to the system alarm services.
     private AlarmManager alarmMgr;
     // The pending intent that is triggered when the alarm fires.
     private PendingIntent alarmIntent;
-  
+
     @Override
-    public void onReceive(Context context, Intent intent) {   
+    public void onReceive(Context context, Intent intent) {
         // BEGIN_INCLUDE(alarm_onreceive)
         /* 
          * If your receiver intent includes extras that need to be passed along to the
@@ -39,7 +40,7 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
          * This intent holds an extra identifying the wake lock.
          */
         Intent service = new Intent(context, SampleSchedulingService.class);
-        
+
         // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, service);
         // END_INCLUDE(alarm_onreceive)
@@ -54,50 +55,13 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
     public void setAlarm(Context context) {
         alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, SampleAlarmReceiver.class);
-        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        // Set the alarm's trigger time to 8:30 a.m.
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
-        calendar.set(Calendar.MINUTE, 1);
-  
-        /* 
-         * If you don't have precise time requirements, use an inexact repeating alarm
-         * the minimize the drain on the device battery.
-         * 
-         * The call below specifies the alarm type, the trigger time, the interval at
-         * which the alarm is fired, and the alarm's associated PendingIntent.
-         * It uses the alarm type RTC_WAKEUP ("Real Time Clock" wake up), which wakes up 
-         * the device and triggers the alarm according to the time of the device's clock. 
-         * 
-         * Alternatively, you can use the alarm type ELAPSED_REALTIME_WAKEUP to trigger 
-         * an alarm based on how much time has elapsed since the device was booted. This 
-         * is the preferred choice if your alarm is based on elapsed time--for example, if 
-         * you simply want your alarm to fire every 60 minutes. You only need to use 
-         * RTC_WAKEUP if you want your alarm to fire at a particular date/time. Remember 
-         * that clock-based time may not translate well to other locales, and that your 
-         * app's behavior could be affected by the user changing the device's time setting.
-         * 
-         * Here are some examples of ELAPSED_REALTIME_WAKEUP:
-         * 
-         * // Wake up the device to fire a one-time alarm in one minute.
-         * alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 
-         *         SystemClock.elapsedRealtime() +
-         *         60*1000, alarmIntent);
-         *        
-         * // Wake up the device to fire the alarm in 30 minutes, and every 30 minutes
-         * // after that.
-         * alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, 
-         *         AlarmManager.INTERVAL_HALF_HOUR, 
-         *         AlarmManager.INTERVAL_HALF_HOUR, alarmIntent);
-         */
-        
-        // Set the alarm to fire at approximately 8:30 a.m., according to the device's
-        // clock, and to repeat once a day.
-        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP,  
-                calendar.getTimeInMillis(), 1000, alarmIntent);
-        
+        for (int id = 0; id < 5; id++) {
+            alarmIntent = PendingIntent.getBroadcast(context, id, intent,0);
+            alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+                    getTimeInMillis(id),AlarmManager.INTERVAL_DAY, alarmIntent);
+        }
+
         // Enable {@code SampleBootReceiver} to automatically restart the alarm when the
         // device is rebooted.
         ComponentName receiver = new ComponentName(context, SampleBootReceiver.class);
@@ -105,7 +69,18 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
 
         pm.setComponentEnabledSetting(receiver,
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                PackageManager.DONT_KILL_APP);           
+                PackageManager.DONT_KILL_APP);
+    }
+
+    private long getTimeInMillis(int alarmId) {
+        Calendar calendar = Calendar.getInstance();
+        TimeCal timeCal = new TimeCal(alarmId).invoke();
+        int hour = timeCal.getHour();
+        int minute = timeCal.getMinute();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        return calendar.getTimeInMillis();
     }
     // END_INCLUDE(set_alarm)
 
@@ -119,7 +94,7 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
         if (alarmMgr!= null) {
             alarmMgr.cancel(alarmIntent);
         }
-        
+
         // Disable {@code SampleBootReceiver} so that it doesn't automatically restart the 
         // alarm when the device is rebooted.
         ComponentName receiver = new ComponentName(context, SampleBootReceiver.class);
@@ -128,6 +103,51 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
         pm.setComponentEnabledSetting(receiver,
                 PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
+    }
+
+    private class TimeCal {
+        private int mAlarmId;
+        private int mHour;
+        private int mMinute;
+
+        public TimeCal(int alarmId) {
+            mAlarmId = alarmId;
+        }
+
+        public int getHour() {
+            return mHour;
+        }
+
+        public int getMinute() {
+            return mMinute;
+        }
+
+        public TimeCal invoke() {
+            mHour = 0;
+            mMinute = 0;
+            switch (mAlarmId){
+                case 0:
+                    mHour =startTime;
+                    break;
+                case 1:
+                    mHour =startTime+3;
+                    mMinute =30;
+                    break;
+                case 2:
+                    mHour =startTime+5;
+                    mMinute =30;
+                    break;
+                case 3:
+                    mHour =startTime+9;
+                    mMinute =30;
+                    break;
+                case 4:
+                    mHour =startTime+12;
+                    mMinute =0;
+                    break;
+            }
+            return this;
+        }
     }
     // END_INCLUDE(cancel_alarm)
 }
